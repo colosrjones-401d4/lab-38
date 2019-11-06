1ec4ae41f4b9567008f2bb398659e7d7344dce4d
ColoSRJones <colosrjones@gmail.com>
2019-11-06 13:44:15 -0600

    api connected

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from './middleware/logger';
+import thunk from './middleware/thunk';
 
 import { reducer as todoList } from './todoList/todoList-reducer';
 import { reducer as details } from './details/details-reducer';
@@ -12,5 +14,5 @@ let reducer = combineReducers({
 });
 
 export default function() {
-  return createStore(reducer, composeWithDevTools());
+  return createStore(reducer, composeWithDevTools(applyMiddleware(logger, thunk)));
 }
